f56a1491d5eed4c67858a94fd8f65f71dc6df84e
Parikshit Hooda <phooda804@live.com>
2018-07-28 23:51:25 +0530

    added gitignore,index.js script, keypadapproach,swapno

---
+++ keypadApproach.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int keypadApproach(string s, string output[]) {
+   if(s.empty())
+     return 0;
+   int digit = stoi(s)
+ }
+
+int main() {
+	// your code goes here
+    int n;
+    int outputLen = 0;
+    string *output = new string[outputLen];
+    for(int i = 0 ; i < outputLen ; i++)
+       cout<<keypadApproach(s,output)<<endl;
+	return 0;
+}
---
+++ swap_no.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int num1=5,num2=6;
+  num2=6;
+  num1=5;
+  cout<<num1<<" "<<num2;
+
+}
