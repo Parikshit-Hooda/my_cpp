9b6cb4a7495f863d418d354d644ffcb71289f888
Parikshit Hooda <phooda804@live.com>
2018-07-28 23:52:39 +0530

    refactor index.js

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ var prompt = require("prompt");
 prompt.start();
 prompt.get(['write_commit'], function(err, result) {
     console.log(result.write_commit);
-    //  var command = `git add . && git commit -m '${result.write_commit}' && git push origin master && exit`;
-    nrc.run('mkdir foo');
-    // console.log(`git add . && git commit -m '${result.write_commit}' && git push origin master && exit`);
     nrc.run(['git add .', `git commit -m "${result.write_commit}"`, 'git push origin master']);
 });
