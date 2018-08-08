a39125ed8acb45d2223719f195d611ce610197d3
Parikshit Hooda <phooda804@live.com>
2018-08-08 18:31:23 +0530

    add lllength.cpp

---
+++ llLength.cpp
@@ -0,0 +1,31 @@
+/**********
+ * Following is the Node class that is already written.
+ 
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+ 
+*********/
+
+int length(Node *head) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input is handled automatically.
+     */
+  Node *temp = head;
+  int llLength = 0;
+  while(temp != NULL) {
+    llLength++;
+    temp=temp->next;
+  }
+    return llLength;
+}
+
+
