2ada9068edc2ec182f83b4a1c3fbaf347e0365f5
Parikshit Hooda <phooda804@live.com>
2022-02-05 19:40:15 +0530

    Time: 15 ms (49.44%), Space: 11.8 MB (45.64%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -4,30 +4,25 @@ public:
         
         if(prices.size() < 2) return 0;
         int psize = prices.size();
-        vector<vector<int>> dp(k+1, vector<int>(psize, 0));
+        vector<vector<int>> dpTxnDay(k+1, vector<int>(psize, 0));
         
         
         for(int t = 1; t <= k ; t++) {
             int currmax = INT_MIN;
             
             for(int d = 1; d < psize; d++) {
-                if(dp[t-1][d-1] - prices[d-1] > currmax) {
-                    currmax = dp[t-1][d-1] - prices[d-1];
+                if(dpTxnDay[t-1][d-1] - prices[d-1] > currmax) {
+                    currmax = dpTxnDay[t-1][d-1] - prices[d-1];
                 }
                 
-                
-                if(dp[t][d-1] < currmax + prices[d]) {
-                    dp[t][d] = currmax + prices[d];
+                if(dpTxnDay[t][d-1] < currmax + prices[d]) {
+                    dpTxnDay[t][d] = currmax + prices[d];
                 } else {
-                    dp[t][d] = dp[t][d-1];
+                    dpTxnDay[t][d] = dpTxnDay[t][d-1];
                 }
             }
-            
-            
         }
-        
-        
-        return dp[k][psize-1];
+        return dpTxnDay[k][psize-1];
         
     }
 };
