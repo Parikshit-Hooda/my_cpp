3bedc1e29f0920be045b219996c0b8793b701348
Parikshit Hooda <phooda804@live.com>
2023-04-15 03:18:47 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (81.00%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<vector<int>> output(rowIndex+1);
-        for(int i=0; i<=rowIndex; i++){
-            output[i].resize(i+1, 1);
-            for(int j=1; j<i; j++){
-                output[i][j] = output[i-1][j-1] + output[i-1][j];
-            }
+        if(rowIndex==0) return {1};
+        if(rowIndex==1) return {1,1};
+        vector<int> result(rowIndex+1, 1);
+        int m=rowIndex/2;  //getting the mid element index as it's zero based index. Had it been N given then (N+1)/2
+        result[1]=rowIndex;
+        for(int i=2; i<=m; i++ ){
+            result[i] =( ((long)result[i-1]) * (rowIndex-i+1) )/i; //(used (long) to avoid Integer OverFlow)
         }
-        return output.back();
+        int r=rowIndex;
+        int l=0;
+        while(r>l) result[r--]=result[l++]; // rth value to (N-r)
+        return result;
+        
     }
 };
