809a5b10cc3286169afd11c023a80b0aac218177
Parikshit Hooda <phooda804@live.com>
2023-05-13 01:44:59 +0530

    Time: 48 ms (39.37%), Space: 12.1 MB (77.04%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int minDeletionSize(vector<string>& strs) {
-        int result = 0;
-
-        for(int i=0; i<strs[0].size(); ++i)
-            for(int j=1; j<strs.size(); ++j)
-                if(strs[j][i] < strs[j-1][i]){
-                    ++result;
+    int minDeletionSize(vector<string>& strs) 
+    {
+        int n = strs.size();
+        int m = strs[0].size();
+        
+        int ans=0;
+        
+        // just check for each column, and if we got wrong lexicographically order just remove that and incraese count by 1
+        for(int j=0;j<m;j++)        // each column
+        {
+            for(int i=1;i<n;i++)    // each row
+            {
+                if(strs[i-1][j] > strs[i][j])
+                {
+                    ans++;
                     break;
                 }
+            }
+        }
         
-        return result;
+        return ans;
     }
 };
