5d1c922b53fd76e801a648d52c491be2b230937b
Parikshit Hooda <phooda804@live.com>
2023-06-03 01:38:44 +0530

    Time: 8 ms (55.36%), Space: 7.2 MB (13.85%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char> mp;
-        vector<int> v(26, 0);
-        bool res = true;
-        if(s.size() != t.size()) return false;
-        
-        for(int i = 0; i < s.size(); i++) {
-            int currs = s[i], currt = t[i];
-            //trace back for difference
-            for(int j = i-1; j >= 0; j--) {
-                if(s[j] == currs || t[j] == currt) 
-                    { 
-                        if(t[j] != currt || s[j] != currs) {res = false; break;}
-                    }
+        map<char,char> map;
+        set<char> set;
+        int n = s.size()-1;
+        while(n>=0){
+            if(map.count(s[n]) && map[s[n]] != t[n]){
+                return false;
             }
-            
+            if( !map.count(s[n]) && set.count(t[n])){
+                return false;
+            }
+            set.insert(t[n]);
+            map[s[n]]=t[n];
+            n--;
         }
-        
-        return res;
-        
+        return true;
     }
 };
