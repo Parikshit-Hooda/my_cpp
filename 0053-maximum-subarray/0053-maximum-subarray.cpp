9af2b65e13d35e469ad59617b7a8991f51b4ddfb
Parikshit Hooda <phooda804@live.com>
2023-06-24 06:20:29 +0530

    Time: 110 ms (93.42%), Space: 67.7 MB (39.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& arr) {
-        int csum = arr[0];
-        int maxSum = arr[0];
-
-        for (int i = 1; i < arr.size(); i++)
-        {
-          if (csum < 0)
-          {
-            csum = arr[i];
-          }
-          else
-          {
-            csum = csum + arr[i];
-          }
-          if (maxSum < csum)
-          {
-            maxSum = csum;
-          }
+        
+        if(arr.size() == 0) return 0;
+        
+        int csum=arr[0], msum = arr[0];
+        
+        for(int i = 1; i < arr.size(); i++) {
+            int ele = arr[i];
+            int sum1 = ele, sum2 = csum+ele;
+            csum = max(sum1,sum2);
+            msum = max(csum, msum);
+            
         }
-        return maxSum;
+        return msum;
+        
+//         int csum = arr[0];
+//         int maxSum = arr[0];
+
+//         for (int i = 1; i < arr.size(); i++)
+//         {
+//           if (csum < 0)
+//           {
+//             csum = arr[i];
+//           }
+//           else
+//           {
+//             csum = csum + arr[i];
+//           }
+//           if (maxSum < csum)
+//           {
+//             maxSum = csum;
+//           }
+//         }
+//         return maxSum;
     }
 };
