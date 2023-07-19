e8f8f15b4f89b81351052bcf21e1325ebf0d4756
Parikshit Hooda <phooda804@live.com>
2023-07-19 11:18:15 +0530

    Time: 3 ms (50.66%), Space: 7.1 MB (19.65%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -1,44 +1,76 @@
 class Solution {
 public:
     
-    void processWord(string &curr, string &res) {
-        if(curr.size() < 3) {
-            for(auto ch: curr) {
-                ch = tolower(ch);
-               res = res + ch; 
+    string capitalizeTitle(string title) {
+        vector<string> words;
+        stringstream ss(title);
+        string word;
+
+        while (ss >> word) {
+            if (word.length() <= 2) {
+                for (char& c : word) {
+                    c = tolower(c);
+                }
+            } else {
+                word[0] = toupper(word[0]);
+                for (size_t i = 1; i < word.length(); ++i) {
+                    word[i] = tolower(word[i]);
+                }
             }
-        } else {
-            char curr0 = toupper(curr[0]);
-            res = res + curr0;
-              for(int i = 1; i < curr.size(); i++) {
-                  char ch = curr[i];
-                  ch = tolower(ch);
-               res = res +  ch;
-            }          
+            words.push_back(word);
         }
-        
-        return ;
+
+        string capitalized_title;
+        for (const auto& w : words) {
+            capitalized_title += w + " ";
+        }
+
+        // Remove the extra space at the end
+        if (!capitalized_title.empty()) {
+            capitalized_title.pop_back();
+        }
+
+        return capitalized_title;
     }
     
-    string capitalizeTitle(string title) {
+//     void processWord(string &curr, string &res) {
+//         if(curr.size() < 3) {
+//             for(auto ch: curr) {
+//                 ch = tolower(ch);
+//                res = res + ch; 
+//             }
+//         } else {
+//             char curr0 = toupper(curr[0]);
+//             res = res + curr0;
+//               for(int i = 1; i < curr.size(); i++) {
+//                   char ch = curr[i];
+//                   ch = tolower(ch);
+//                res = res +  ch;
+//             }          
+//         }
         
-        string res = "";
+//         return ;
+//     }
+    
+//     string capitalizeTitle(string title) {
         
-        string curr = "";
+//         string res = "";
         
-        for(int i = 0; i < title.size(); i++) {
-            if(title[i] == ' ') {
-                //create curr
-                processWord(curr, res);
-                curr = "";
-                res = res + ' ';
-            } else {
-                curr = curr + title[i];
-            }
-        }
-                processWord(curr, res);
+//         string curr = "";
         
-        return res;
+//         for(int i = 0; i < title.size(); i++) {
+//             if(title[i] == ' ') {
+//                 //create curr
+//                 processWord(curr, res);
+//                 curr = "";
+//                 res = res + ' ';
+//             } else {
+//                 curr = curr + title[i];
+//             }
+//         }
+//                 processWord(curr, res);
         
-    }
+//         return res;
+        
+//     }
 };
