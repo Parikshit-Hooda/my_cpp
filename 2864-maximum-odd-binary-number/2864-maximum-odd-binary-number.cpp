c4253aa7d443e8fc58a650747dcfc2c7f2f6d3fd
Parikshit Hooda <phooda804@live.com>
2023-10-06 07:20:04 +0530

    Time: 12 ms (5.45%), Space: 12.8 MB (5.43%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -14,14 +14,14 @@ public:
         int lastidx = s.size()-1;
         
         
-        string ones(c1-1, '1');
-        string zeros(c0, '0');
-        res = ones + zeros + '1';
+        // string ones(c1-1, '1');
+        // string zeros(c0, '0');
+        // res = ones + zeros + '1';
         
         // for(int i = 0 ; i < s.size(); i++) {
-            // while(c1>1) {res = res + '1'; c1--;}
-            // while(c0--) { res = res + '0'; }
-            // res = res + '1';
+            while(c1>1) {res = res + '1'; c1--;}
+            while(c0--) { res = res + '0'; }
+            res = res + '1';
         // }
         
         // for(int i = 0; i < res.size(); i++) {
