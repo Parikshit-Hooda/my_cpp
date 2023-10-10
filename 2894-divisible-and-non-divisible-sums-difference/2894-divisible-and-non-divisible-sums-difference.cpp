b8f549e554fac1379c69a7d4a110d987118bfbbb
Parikshit Hooda <phooda804@live.com>
2023-10-10 07:36:01 +0530

    Time: 4 ms (31.61%), Space: 6.4 MB (41.42%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int n1=0,n2=0;
         
-        for(int i = 1; i <= n; i++) {
-            if(i%m == 0) n2+=i;
-            if(i%m != 0) n1+=i;
-            
-        }
-        
-        return n1-n2;
+        return n*(n+1)/2 - 2*m*(n/m)*(n/m+1)/2;
     }
 };
