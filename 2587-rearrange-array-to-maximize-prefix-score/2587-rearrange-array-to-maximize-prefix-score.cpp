49567dbd55c10e828af6c99681e55f8ceb62f666
Parikshit Hooda <phooda804@live.com>
2023-10-13 02:08:51 +0530

    Time: 126 ms (61.69%), Space: 83.6 MB (30.58%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
@@ -1,17 +1,16 @@
 class Solution {
-public:
-    int maxScore(vector<int>& nums) {
-        vector<int> prefix(nums.size(), 0);
-        sort(nums.begin(), nums.end(), greater<>());
-        int ans=0;
-        long long curr = nums[0];
-        if (curr <= 0) return ans;
-        ans++;
-        for(int i = 1; i < nums.size(); i++) {
-            curr += nums[i];
-            if(curr > 0) ans++;
-        }
-        
-        return ans;
-    }
-};
+	public:
+		int maxScore(vector<int>& nums) {
+			int n = nums.size();
+			int ans = 0;
+			long long sum = 0;
+			sort(nums.begin(),nums.end(),greater<int>());
+
+			for(int i=0;i<n;i++){
+				sum+=nums[i];
+				if(sum > 0) ans++;
+			}
+
+			return ans;
+		}
+	};
